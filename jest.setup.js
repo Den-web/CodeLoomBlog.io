b0824a915fdbfc6123d254e244a796fcb39e0b33
Den-web <kirevden@gmail.com>
2025-03-12 20:45:05 +0100

    feat: add dev tools and testing setup

---
+++ jest.setup.js
@@ -0,0 +1,45 @@
+// Learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom'
+
+// Mock next/navigation
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+      replace: jest.fn(),
+      prefetch: jest.fn(),
+      back: jest.fn(),
+    }
+  },
+  usePathname() {
+    return ''
+  },
+  useSearchParams() {
+    return new URLSearchParams()
+  },
+}))
+
+// Mock next/image
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props) => {
+    // eslint-disable-next-line jsx-a11y/alt-text
+    return <img {...props} />
+  },
+}))
+
+// Mock next-themes
+jest.mock('next-themes', () => ({
+  useTheme() {
+    return {
+      theme: 'light',
+      setTheme: jest.fn(),
+    }
+  },
+}))
+
+// Mock contentlayer
+jest.mock('contentlayer/generated', () => ({
+  allPosts: [],
+  allPages: [],
+})) 
